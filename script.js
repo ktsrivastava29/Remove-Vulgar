8a122016ddaecd4b5039dc9b6a366eab4d066fbf
Tanmay Srivastava <ktsrivastava29@gmail.com>
2021-07-27 16:08:35 +0530

    Added a few lines in js file

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ function replaceText(element) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
         element.textContent = element.textContent.replace(/fuck/gi, " 🥺")
+        element.textContent = element.textContent.replace(/Fuck/gi, " 🥺")
         element.textContent = element.textContent.replace(/fucking/gi, " 🥺")
+        element.textContent = element.textContent.replace(/Fucking/gi, " 🥺")
         element.textContent = element.textContent.replace(/porn/gi, " 🥺")
+        element.textContent = element.textContent.replace(/Porn/gi, " 🥺")
         element.textContent = element.textContent.replace(/sex/gi, " 🥺")
+        element.textContent = element.textContent.replace(/Sex/gi, " 🥺")
+        element.textContent = element.textContent.replace(/Fucked/gi, " 🥺")
+        element.textContent = element.textContent.replace(/fucked/gi, " 🥺")
     }
 }
