aeaa81b772c0dd5baaf6e4b5e4ca2361db3fd13f
Prashant Thakur <prashant.th02@gmail.com>
2021-07-28 18:36:25 +0530

    Added some more bad words in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        const badWords = ["fuck","porn","sex"];
+        const badWords = ["fuck","porn","sex", "Tits", "Shit", "Bullshit", "Dick", "Dickhead", "Pussy", "Motherfucker", "Bastard"];
         badWords.forEach( e =>
             {
                 const badRegex=new RegExp(e,'gi')
